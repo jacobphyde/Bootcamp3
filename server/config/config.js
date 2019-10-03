b2980eaf84bbc5f6d7fc86af760938f0f13de8a7
jacobphyde <jacob.hyde@ufl.edu>
2019-10-02 21:17:05 -0400

    first commit

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    db: {
+      uri: 'mongodb+srv://user:pass@cluster0-dmwh1.mongodb.net/test?retryWrites=true&w=majority'//place the URI of your mongo database here.
+    }, 
+    openCage: {
+      key: 'b912e38e6e4f46cfadfdd22f613e1c21' //place your openCage public key here - Sign-up for a free key https://opencagedata.com/
+    },
+    port: 8080
+  };
